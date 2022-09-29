901f03288fb4e1fe487819df0e9e722f88260c75
creoii <33011337+creoii@users.noreply.github.com>
2022-09-28 21:06:19 -0400

    generation util

--- src/main/java/com/github/creoii/greatbigworld/main/GreatBigWorld.java
+++ src/main/java/com/github/creoii/greatbigworld/main/GreatBigWorld.java
@@ -3,9 +3,11 @@ package com.github.creoii.greatbigworld.main;
 import com.github.creoii.greatbigworld.main.registry.*;
 import com.github.creoii.greatbigworld.main.util.Register;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.math.random.Random;
 
 public class GreatBigWorld implements ModInitializer {
     public static final String NAMESPACE = "great_big_world";
+    public static final Random RANDOM = Random.createLocal();
     public static final Register[] REGISTERS = new Register[]{new BlockRegistry(), new ItemRegistry(), new PlacerRegistry(), new ConfiguredFeatureRegistry(), new PlacedFeatureRegistry()};
 
     @Override
--- src/main/java/com/github/creoii/greatbigworld/main/util/Register.java
+++ src/main/java/com/github/creoii/greatbigworld/main/util/Register.java
@@ -1,6 +1,10 @@
 package com.github.creoii.greatbigworld.main.util;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
 public interface Register {
     void register();
+    @Environment(EnvType.CLIENT)
     default void registerClient() { }
 }
