5f1c8457898d0af8f83030ebf108419825a5e74f
creoii <33011337+creoii@users.noreply.github.com>
2022-09-30 20:09:20 -0400

    Glimmering Mushroom Generation
    fix texture mapping for Glimmering Mushrooms
    add vines to Mahogany Trees

--- src/main/java/com/github/creoii/greatbigworld/main/registry/ConfiguredFeatureRegistry.java
+++ src/main/java/com/github/creoii/greatbigworld/main/registry/ConfiguredFeatureRegistry.java
@@ -1,9 +1,13 @@
 package com.github.creoii.greatbigworld.main.registry;
 
+import com.github.creoii.greatbigworld.block.GlimmeringMushroomBlock;
 import com.github.creoii.greatbigworld.main.util.Register;
 import com.github.creoii.greatbigworld.world.decorator.BranchTreeDecorator;
 import com.github.creoii.greatbigworld.world.placer.AspenFoliagePlacer;
 import com.github.creoii.greatbigworld.world.placer.TwistingTrunkPlacer;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.collection.DataPool;
 import net.minecraft.util.math.intprovider.ConstantIntProvider;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.registry.RegistryEntry;
@@ -11,22 +15,27 @@ import net.minecraft.world.gen.feature.*;
 import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize;
 import net.minecraft.world.gen.foliage.AcaciaFoliagePlacer;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
+import net.minecraft.world.gen.stateprovider.WeightedBlockStateProvider;
+import net.minecraft.world.gen.treedecorator.LeavesVineTreeDecorator;
 import net.minecraft.world.gen.trunk.StraightTrunkPlacer;
 
 import java.util.List;
-import java.util.Random;
 
 public class ConfiguredFeatureRegistry implements Register {
     public static RegistryEntry<ConfiguredFeature<TreeFeatureConfig, ?>> MAHOGANY;
     public static RegistryEntry<ConfiguredFeature<TreeFeatureConfig, ?>> YELLOW_ASPEN;
     public static RegistryEntry<ConfiguredFeature<TreeFeatureConfig, ?>> GREEN_ASPEN;
+    public static RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> PATCH_DAYLIGHT_MUSHROOMS;
+    public static RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> PATCH_DARKBLIGHT_MUSHROOMS;
 
     @Override
     public void register() { }
 
     static {
-        MAHOGANY = ConfiguredFeatures.register("mahogany", Feature.TREE, new TreeFeatureConfig.Builder(BlockStateProvider.of(BlockRegistry.MAHOGANY.log()), new TwistingTrunkPlacer(3, 2, 0, UniformIntProvider.create(4, 6), UniformIntProvider.create(1, 2), UniformIntProvider.create(2, 3), UniformIntProvider.create(1, 2), false), BlockStateProvider.of(BlockRegistry.MAHOGANY_LEAVES), new AcaciaFoliagePlacer(ConstantIntProvider.create(3), ConstantIntProvider.create(1)), new TwoLayersFeatureSize(9, 2, 6)).ignoreVines().build());
+        MAHOGANY = ConfiguredFeatures.register("mahogany", Feature.TREE, new TreeFeatureConfig.Builder(BlockStateProvider.of(BlockRegistry.MAHOGANY.log()), new TwistingTrunkPlacer(3, 2, 0, UniformIntProvider.create(4, 6), UniformIntProvider.create(1, 2), UniformIntProvider.create(2, 3), UniformIntProvider.create(1, 2), false), BlockStateProvider.of(BlockRegistry.MAHOGANY_LEAVES), new AcaciaFoliagePlacer(ConstantIntProvider.create(3), ConstantIntProvider.create(1)), new TwoLayersFeatureSize(9, 2, 6)).decorators(List.of(new LeavesVineTreeDecorator(.1f))).ignoreVines().build());
         YELLOW_ASPEN = ConfiguredFeatures.register("yellow_aspen", Feature.TREE, new TreeFeatureConfig.Builder(BlockStateProvider.of(BlockRegistry.ASPEN.log()), new StraightTrunkPlacer(6, 3, 1), BlockStateProvider.of(BlockRegistry.YELLOW_ASPEN_LEAVES), new AspenFoliagePlacer(ConstantIntProvider.create(2), UniformIntProvider.create(2, 3), UniformIntProvider.create(2, 3)), new TwoLayersFeatureSize(2, 0, 1)).decorators(List.of(new BranchTreeDecorator(BlockRegistry.ASPEN.log().getDefaultState(), .1f, false, UniformIntProvider.create(3, 6)))).build());
         GREEN_ASPEN = ConfiguredFeatures.register("green_aspen", Feature.TREE, new TreeFeatureConfig.Builder(BlockStateProvider.of(BlockRegistry.ASPEN.log()), new StraightTrunkPlacer(6, 3, 1), BlockStateProvider.of(BlockRegistry.GREEN_ASPEN_LEAVES), new AspenFoliagePlacer(ConstantIntProvider.create(2), UniformIntProvider.create(2, 3), UniformIntProvider.create(2, 3)), new TwoLayersFeatureSize(2, 0, 1)).decorators(List.of(new BranchTreeDecorator(BlockRegistry.ASPEN.log().getDefaultState(), .1f, false, UniformIntProvider.create(3, 6)))).build());
+        PATCH_DAYLIGHT_MUSHROOMS = ConfiguredFeatures.register("patch_daylight_mushrooms", Feature.RANDOM_PATCH, ConfiguredFeatures.createRandomPatchFeatureConfig(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(new WeightedBlockStateProvider(DataPool.<BlockState>builder().add(BlockRegistry.DAYLIGHT_MUSHROOM.getDefaultState().with(GlimmeringMushroomBlock.MUSHROOMS, 1), 6).add(BlockRegistry.DAYLIGHT_MUSHROOM.getDefaultState().with(GlimmeringMushroomBlock.MUSHROOMS, 2), 4).add(BlockRegistry.DAYLIGHT_MUSHROOM.getDefaultState().with(GlimmeringMushroomBlock.MUSHROOMS, 3), 2).build())), List.of(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.COARSE_DIRT, Blocks.PODZOL)));
+        PATCH_DARKBLIGHT_MUSHROOMS = ConfiguredFeatures.register("patch_darkblight_mushrooms", Feature.RANDOM_PATCH, ConfiguredFeatures.createRandomPatchFeatureConfig(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(new WeightedBlockStateProvider(DataPool.<BlockState>builder().add(BlockRegistry.DARKBLIGHT_MUSHROOM.getDefaultState().with(GlimmeringMushroomBlock.MUSHROOMS, 1), 6).add(BlockRegistry.DARKBLIGHT_MUSHROOM.getDefaultState().with(GlimmeringMushroomBlock.MUSHROOMS, 2), 4).add(BlockRegistry.DARKBLIGHT_MUSHROOM.getDefaultState().with(GlimmeringMushroomBlock.MUSHROOMS, 3), 2).build())), List.of(Blocks.STONE, Blocks.DEEPSLATE, Blocks.GRANITE, Blocks.ANDESITE, Blocks.DIORITE, Blocks.OAK_PLANKS, Blocks.DIRT, Blocks.TUFF, Blocks.CALCITE)));
     }
 }
--- src/main/java/com/github/creoii/greatbigworld/main/registry/PlacedFeatureRegistry.java
+++ src/main/java/com/github/creoii/greatbigworld/main/registry/PlacedFeatureRegistry.java
@@ -6,12 +6,10 @@ import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.Heightmap;
+import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.*;
-import net.minecraft.world.gen.placementmodifier.BiomePlacementModifier;
-import net.minecraft.world.gen.placementmodifier.HeightmapPlacementModifier;
-import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
-import net.minecraft.world.gen.placementmodifier.SurfaceWaterDepthFilterPlacementModifier;
+import net.minecraft.world.gen.placementmodifier.*;
 
 public class PlacedFeatureRegistry implements Register {
     public static RegistryEntry<PlacedFeature> MAHOGANY;
@@ -22,12 +20,16 @@ public class PlacedFeatureRegistry implements Register {
     public static RegistryEntry<PlacedFeature> TREES_ASPEN_YELLOW_SPARSE;
     public static RegistryEntry<PlacedFeature> TREES_ASPEN_GREEN_DENSE;
     public static RegistryEntry<PlacedFeature> TREES_ASPEN_GREEN_SPARSE;
+    public static RegistryEntry<PlacedFeature> PATCH_DAYLIGHT_MUSHROOMS;
+    public static RegistryEntry<PlacedFeature> PATCH_DARKBLIGHT_MUSHROOMS;
 
     @Override
     public void register() {
         if (TREES_MAHOGANY.getKey().isPresent()) BiomeModifications.addFeature(BiomeSelectors.tag(Tags.BiomeTags.MAHOGANY_BIOMES), GenerationStep.Feature.VEGETAL_DECORATION, TREES_MAHOGANY.getKey().get());
         if (TREES_ASPEN_YELLOW_SPARSE.getKey().isPresent()) BiomeModifications.addFeature(BiomeSelectors.tag(Tags.BiomeTags.YELLOW_ASPEN_BIOMES), GenerationStep.Feature.VEGETAL_DECORATION, TREES_ASPEN_YELLOW_SPARSE.getKey().get());
         if (TREES_ASPEN_GREEN_SPARSE.getKey().isPresent()) BiomeModifications.addFeature(BiomeSelectors.tag(Tags.BiomeTags.GREEN_ASPEN_BIOMES), GenerationStep.Feature.VEGETAL_DECORATION, TREES_ASPEN_GREEN_SPARSE.getKey().get());
+        if (PATCH_DAYLIGHT_MUSHROOMS.getKey().isPresent()) BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.OLD_GROWTH_SPRUCE_TAIGA, BiomeKeys.OLD_GROWTH_PINE_TAIGA), GenerationStep.Feature.VEGETAL_DECORATION, PATCH_DAYLIGHT_MUSHROOMS.getKey().get());
+        if (PATCH_DARKBLIGHT_MUSHROOMS.getKey().isPresent()) BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.VEGETAL_DECORATION, PATCH_DARKBLIGHT_MUSHROOMS.getKey().get());
     }
 
     static {
@@ -39,5 +41,7 @@ public class PlacedFeatureRegistry implements Register {
         TREES_ASPEN_YELLOW_SPARSE = PlacedFeatures.register("trees_aspen_yellow_sparse", ConfiguredFeatureRegistry.YELLOW_ASPEN, PlacedFeatures.createCountExtraModifier(0, .25f, 1), SquarePlacementModifier.of(), HeightmapPlacementModifier.of(Heightmap.Type.OCEAN_FLOOR_WG), SurfaceWaterDepthFilterPlacementModifier.of(0), BiomePlacementModifier.of());
         TREES_ASPEN_GREEN_DENSE = PlacedFeatures.register("trees_aspen_green_dense", ConfiguredFeatureRegistry.GREEN_ASPEN, PlacedFeatures.createCountExtraModifier(1, .1f, 1), SquarePlacementModifier.of(), HeightmapPlacementModifier.of(Heightmap.Type.OCEAN_FLOOR_WG), SurfaceWaterDepthFilterPlacementModifier.of(0), BiomePlacementModifier.of());
         TREES_ASPEN_GREEN_SPARSE = PlacedFeatures.register("trees_aspen_green_sparse", ConfiguredFeatureRegistry.GREEN_ASPEN, PlacedFeatures.createCountExtraModifier(0, .25f, 1), SquarePlacementModifier.of(), HeightmapPlacementModifier.of(Heightmap.Type.OCEAN_FLOOR_WG), SurfaceWaterDepthFilterPlacementModifier.of(0), BiomePlacementModifier.of());
+        PATCH_DAYLIGHT_MUSHROOMS = PlacedFeatures.register("patch_daylight_mushrooms", ConfiguredFeatureRegistry.PATCH_DAYLIGHT_MUSHROOMS, RarityFilterPlacementModifier.of(10), SquarePlacementModifier.of(), PlacedFeatures.WORLD_SURFACE_WG_HEIGHTMAP, BiomePlacementModifier.of());
+        PATCH_DARKBLIGHT_MUSHROOMS = PlacedFeatures.register("patch_darkblight_mushrooms", ConfiguredFeatureRegistry.PATCH_DARKBLIGHT_MUSHROOMS, CountPlacementModifier.of(2), SquarePlacementModifier.of(), PlacedFeatures.BOTTOM_TO_120_RANGE, BiomePlacementModifier.of());
     }
 }
