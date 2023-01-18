e28d4f3b87f09e6d89f1e2fdb72caab999eb9c12
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-01-18 10:48:40 -0500

    Fix Models & Swamp Pyramid Processor
    Added Mud & Swamp Moss Patch for later
    Swamp Pyramid Processor can set air now

--- src/main/java/com/github/creoii/greatbigworld/world/processor/SwampPyramidStructureProcessor.java
+++ src/main/java/com/github/creoii/greatbigworld/world/processor/SwampPyramidStructureProcessor.java
@@ -5,6 +5,7 @@ import com.github.creoii.greatbigworld.main.registry.StructureRegistry;
 import com.google.common.collect.Maps;
 import com.mojang.serialization.Codec;
 import net.minecraft.block.*;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.structure.StructurePlacementData;
 import net.minecraft.structure.StructureTemplate;
 import net.minecraft.structure.processor.StructureProcessor;
@@ -14,42 +15,45 @@ import net.minecraft.util.collection.DataPool;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.WorldView;
+import net.minecraft.world.gen.stateprovider.BlockStateProvider;
+import net.minecraft.world.gen.stateprovider.WeightedBlockStateProvider;
 
 import java.util.Map;
-import java.util.Optional;
 
 public class SwampPyramidStructureProcessor extends StructureProcessor {
     public static final SwampPyramidStructureProcessor INSTANCE = new SwampPyramidStructureProcessor();
     public static final Codec<SwampPyramidStructureProcessor> CODEC = Codec.unit(() -> INSTANCE);
-    private final Map<Block, DataPool<Block>> replacementMap = Util.make(Maps.newHashMap(), (replacements) -> {
-        replacements.put(BlockRegistry.COBBLESTONE_BRICKS, DataPool.<Block>builder().add(Blocks.COBBLESTONE, 1).add(Blocks.MOSSY_COBBLESTONE, 3).add(BlockRegistry.MOSSY_COBBLESTONE_BRICKS, 5).build());
-        replacements.put(BlockRegistry.COBBLESTONE_BRICK_SLAB, DataPool.<Block>builder().add(Blocks.COBBLESTONE_SLAB, 1).add(Blocks.MOSSY_COBBLESTONE_SLAB, 3).add(BlockRegistry.MOSSY_COBBLESTONE_BRICK_SLAB, 5).build());
-        replacements.put(BlockRegistry.COBBLESTONE_BRICK_STAIRS, DataPool.<Block>builder().add(Blocks.COBBLESTONE_STAIRS, 1).add(Blocks.MOSSY_COBBLESTONE_STAIRS, 3).add(BlockRegistry.MOSSY_COBBLESTONE_BRICK_STAIRS, 5).build());
-        replacements.put(BlockRegistry.COBBLESTONE_BRICK_WALL, DataPool.<Block>builder().add(Blocks.COBBLESTONE_WALL, 1).add(Blocks.MOSSY_COBBLESTONE_WALL, 3).add(BlockRegistry.MOSSY_COBBLESTONE_BRICK_WALL, 5).build());
+    private final Map<Block, BlockStateProvider> replacementMap = Util.make(Maps.newHashMap(), (replacements) -> {
+        replacements.put(BlockRegistry.COBBLESTONE_BRICKS, new WeightedBlockStateProvider(DataPool.<BlockState>builder().add(Blocks.COBBLESTONE.getDefaultState(), 1).add(Blocks.MOSSY_COBBLESTONE.getDefaultState(), 3).add(BlockRegistry.MOSSY_COBBLESTONE_BRICKS.getDefaultState(), 5).build()));
+        replacements.put(BlockRegistry.COBBLESTONE_BRICK_SLAB, new WeightedBlockStateProvider(DataPool.<BlockState>builder().add(Blocks.COBBLESTONE_SLAB.getDefaultState(), 1).add(Blocks.MOSSY_COBBLESTONE_SLAB.getDefaultState(), 3).add(BlockRegistry.MOSSY_COBBLESTONE_BRICK_SLAB.getDefaultState(), 5).build()));
+        replacements.put(BlockRegistry.COBBLESTONE_BRICK_STAIRS, new WeightedBlockStateProvider(DataPool.<BlockState>builder().add(Blocks.COBBLESTONE_STAIRS.getDefaultState(), 1).add(Blocks.MOSSY_COBBLESTONE_STAIRS.getDefaultState(), 3).add(BlockRegistry.MOSSY_COBBLESTONE_BRICK_STAIRS.getDefaultState(), 5).build()));
+        replacements.put(BlockRegistry.COBBLESTONE_BRICK_WALL, new WeightedBlockStateProvider(DataPool.<BlockState>builder().add(Blocks.COBBLESTONE_WALL.getDefaultState(), 1).add(Blocks.MOSSY_COBBLESTONE_WALL.getDefaultState(), 3).add(BlockRegistry.MOSSY_COBBLESTONE_BRICK_WALL.getDefaultState(), 5).build()));
     });
 
     public StructureTemplate.StructureBlockInfo process(WorldView world, BlockPos pos, BlockPos pivot, StructureTemplate.StructureBlockInfo originalBlockInfo, StructureTemplate.StructureBlockInfo currentBlockInfo, StructurePlacementData data) {
         Random random = data.getRandom(currentBlockInfo.pos);
-        if (random.nextFloat() < .6f) {
-            Optional<Block> optional = replacementMap.get(currentBlockInfo.state.getBlock()).getDataOrEmpty(random);
-            if (optional.isEmpty()) return currentBlockInfo;
-            else {
-                BlockState blockState = currentBlockInfo.state;
-                BlockState blockState2 = optional.get().getDefaultState();
-                if (blockState.contains(StairsBlock.FACING)) {
-                    blockState2 = blockState2.with(StairsBlock.FACING, blockState.get(StairsBlock.FACING));
-                }
-
-                if (blockState.contains(StairsBlock.HALF)) {
-                    blockState2 = blockState2.with(StairsBlock.HALF, blockState.get(StairsBlock.HALF));
-                }
-
-                if (blockState.contains(SlabBlock.TYPE)) {
-                    blockState2 = blockState2.with(SlabBlock.TYPE, blockState.get(SlabBlock.TYPE));
-                }
-
-                return new StructureTemplate.StructureBlockInfo(currentBlockInfo.pos, blockState2, currentBlockInfo.nbt);
+        Block block = currentBlockInfo.state.getBlock();
+        if (random.nextFloat() < .6f && replacementMap.containsKey(block)) {
+            if (random.nextFloat() < .1f) return null;
+            BlockState state = replacementMap.get(block).get(random, currentBlockInfo.pos);
+            BlockState blockState = currentBlockInfo.state;
+            if (blockState.contains(StairsBlock.FACING)) {
+                state = state.with(StairsBlock.FACING, blockState.get(StairsBlock.FACING));
             }
+
+            if (blockState.contains(StairsBlock.HALF)) {
+                state = state.with(StairsBlock.HALF, blockState.get(StairsBlock.HALF));
+            }
+
+            if (blockState.contains(StairsBlock.SHAPE)) {
+                state = state.with(StairsBlock.SHAPE, blockState.get(StairsBlock.SHAPE));
+            }
+
+            if (blockState.contains(SlabBlock.TYPE)) {
+                state = state.with(SlabBlock.TYPE, blockState.get(SlabBlock.TYPE));
+            }
+
+            return new StructureTemplate.StructureBlockInfo(currentBlockInfo.pos, state, currentBlockInfo.nbt);
         }
         return currentBlockInfo;
     }
