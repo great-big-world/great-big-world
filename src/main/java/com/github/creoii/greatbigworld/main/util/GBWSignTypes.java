bb94315f097678875596d0f3d6c8d51e5ca790aa
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-01-20 15:33:50 -0500

    Use New Fabric SignTypeRegistry

--- src/main/java/com/github/creoii/greatbigworld/main/util/GBWSignTypes.java
+++ src/main/java/com/github/creoii/greatbigworld/main/util/GBWSignTypes.java
@@ -1,8 +1,11 @@
 package com.github.creoii.greatbigworld.main.util;
 
+import com.github.creoii.greatbigworld.main.GreatBigWorld;
+import net.fabricmc.fabric.api.object.builder.v1.sign.SignTypeRegistry;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.SignType;
 
 public class GBWSignTypes {
-    public static final SignType MAHOGANY = SignType.register(new SignType("mahogany"));
-    public static final SignType ASPEN = SignType.register(new SignType("aspen"));
+    public static final SignType MAHOGANY = SignTypeRegistry.registerSignType(new Identifier(GreatBigWorld.NAMESPACE, "mahogany"));
+    public static final SignType ASPEN = SignTypeRegistry.registerSignType(new Identifier(GreatBigWorld.NAMESPACE, "aspen"));
 }
