13443aaa6ef068de19f0bb85fa67ec2c1681fc82
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-01-22 14:52:02 -0500

    Increase Thicket Spawns in Pyramid
    Don't hardcode getRandomMask()

--- src/main/java/com/github/creoii/greatbigworld/item/WoodenMaskItem.java
+++ src/main/java/com/github/creoii/greatbigworld/item/WoodenMaskItem.java
@@ -4,31 +4,26 @@ import com.github.creoii.greatbigworld.main.GreatBigWorld;
 import com.github.creoii.greatbigworld.main.registry.ItemRegistry;
 import com.github.creoii.greatbigworld.main.util.Tags;
 import com.github.creoii.greatbigworld.main.util.material.WoodenMaskArmorMaterial;
+import com.google.common.collect.ImmutableList;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WoodenMaskItem extends GBWArmorItem {
+    public static final List<Item> WOODEN_MASKS = new ArrayList<>();
+
     public WoodenMaskItem(WoodenMaskArmorMaterial material) {
         super(material, EquipmentSlot.HEAD, new FabricItemSettings().maxCount(1), enchantment -> {
             return Registries.ENCHANTMENT.getEntry(enchantment).isIn(Tags.EnchantmentTags.MASK_ALLOWED);
         });
+        WOODEN_MASKS.add(this);
     }
 
     public static Item getRandomMask() {
-        return switch (GreatBigWorld.RANDOM.nextInt(11)) {
-            case 0 -> ItemRegistry.SPRUCE_MASK;
-            case 1 -> ItemRegistry.BIRCH_MASK;
-            case 2 -> ItemRegistry.ASPEN_MASK;
-            case 3 -> ItemRegistry.JUNGLE_MASK;
-            case 4 -> ItemRegistry.MAHOGANY_MASK;
-            case 5 -> ItemRegistry.ACACIA_MASK;
-            case 6 -> ItemRegistry.DARK_OAK_MASK;
-            case 7 -> ItemRegistry.MANGROVE_MASK;
-            case 8 -> ItemRegistry.CRIMSON_MASK;
-            case 9 -> ItemRegistry.WARPED_MASK;
-            default -> ItemRegistry.OAK_MASK;
-        };
+        return WOODEN_MASKS.get(GreatBigWorld.RANDOM.nextInt(WOODEN_MASKS.size()));
     }
 }
