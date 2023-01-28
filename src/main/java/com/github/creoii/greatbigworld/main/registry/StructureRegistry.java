829f1ceed0867c0d503086ee98443ea40e80af1b
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-01-28 00:57:07 -0500

    Lighthouse Structure Jsons

--- src/main/java/com/github/creoii/greatbigworld/main/registry/StructureRegistry.java
+++ src/main/java/com/github/creoii/greatbigworld/main/registry/StructureRegistry.java
@@ -13,12 +13,14 @@ import net.minecraft.world.gen.structure.Structure;
 
 public class StructureRegistry implements Register {
     public static RegistryKey<Structure> SWAMP_PYRAMID;
+    public static RegistryKey<Structure> LIGHTHOUSE;
 
     public static final StructureProcessorType<SwampPyramidStructureProcessor> SWAMP_PYRAMID_PROCESSOR = () -> SwampPyramidStructureProcessor.CODEC;
 
     @Override
     public void register() {
         SWAMP_PYRAMID = RegistryKey.of(RegistryKeys.STRUCTURE, new Identifier(GreatBigWorld.NAMESPACE, "swamp_pyramid"));
+        LIGHTHOUSE = RegistryKey.of(RegistryKeys.STRUCTURE, new Identifier(GreatBigWorld.NAMESPACE, "lighthouse"));
         Registry.register(Registries.STRUCTURE_PROCESSOR, new Identifier(GreatBigWorld.NAMESPACE, "swamp_pyramid"), SWAMP_PYRAMID_PROCESSOR);
     }
 }
