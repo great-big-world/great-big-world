1540ffcb90a2ef51984fcafff1eb476fff26fc41
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-01-27 23:11:37 -0500

    Stained Calcite Loot Tables
    Green & Blue textures

--- src/main/java/com/github/creoii/greatbigworld/main/util/datagen/GBWDataGenerator.java
+++ src/main/java/com/github/creoii/greatbigworld/main/util/datagen/GBWDataGenerator.java
@@ -1,13 +1,24 @@
 package com.github.creoii.greatbigworld.main.util.datagen;
 
+import com.github.creoii.greatbigworld.main.GreatBigWorld;
 import com.github.creoii.greatbigworld.main.registry.BlockRegistry;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.SimpleFabricLootTableProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.context.LootContextTypes;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.util.Identifier;
+
+import java.util.function.BiConsumer;
 
 public class GBWDataGenerator implements DataGeneratorEntrypoint {
     @Override
@@ -15,6 +26,7 @@ public class GBWDataGenerator implements DataGeneratorEntrypoint {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
         pack.addProvider(LangProvider::new);
         pack.addProvider(ModelProvider::new);
+        pack.addProvider(LootTableProvider::new);
     }
 
     private static class LangProvider extends FabricLanguageProvider {
@@ -70,6 +82,36 @@ public class GBWDataGenerator implements DataGeneratorEntrypoint {
 
         @Override
         public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        }
+    }
+
+    public static class LootTableProvider extends SimpleFabricLootTableProvider {
+        public LootTableProvider(FabricDataOutput output) {
+            super(output, LootContextTypes.BLOCK);
+        }
+
+        @Override
+        public void accept(BiConsumer<Identifier, LootTable.Builder> biConsumer) {
+            biConsumer.accept(new Identifier(GreatBigWorld.NAMESPACE, "brown_stained_calcite"), drops(BlockRegistry.BROWN_STAINED_CALCITE));
+            biConsumer.accept(new Identifier(GreatBigWorld.NAMESPACE, "red_stained_calcite"), drops(BlockRegistry.RED_STAINED_CALCITE));
+            biConsumer.accept(new Identifier(GreatBigWorld.NAMESPACE, "orange_stained_calcite"), drops(BlockRegistry.ORANGE_STAINED_CALCITE));
+            biConsumer.accept(new Identifier(GreatBigWorld.NAMESPACE, "yellow_stained_calcite"), drops(BlockRegistry.YELLOW_STAINED_CALCITE));
+            biConsumer.accept(new Identifier(GreatBigWorld.NAMESPACE, "lime_stained_calcite"), drops(BlockRegistry.LIME_STAINED_CALCITE));
+            biConsumer.accept(new Identifier(GreatBigWorld.NAMESPACE, "green_stained_calcite"), drops(BlockRegistry.GREEN_STAINED_CALCITE));
+            biConsumer.accept(new Identifier(GreatBigWorld.NAMESPACE, "cyan_stained_calcite"), drops(BlockRegistry.CYAN_STAINED_CALCITE));
+            biConsumer.accept(new Identifier(GreatBigWorld.NAMESPACE, "blue_stained_calcite"), drops(BlockRegistry.BLUE_STAINED_CALCITE));
+            biConsumer.accept(new Identifier(GreatBigWorld.NAMESPACE, "light_blue_stained_calcite"), drops(BlockRegistry.LIGHT_BLUE_STAINED_CALCITE));
+            biConsumer.accept(new Identifier(GreatBigWorld.NAMESPACE, "pink_stained_calcite"), drops(BlockRegistry.PINK_STAINED_CALCITE));
+            biConsumer.accept(new Identifier(GreatBigWorld.NAMESPACE, "magenta_stained_calcite"), drops(BlockRegistry.MAGENTA_STAINED_CALCITE));
+            biConsumer.accept(new Identifier(GreatBigWorld.NAMESPACE, "purple_stained_calcite"), drops(BlockRegistry.PURPLE_STAINED_CALCITE));
+            biConsumer.accept(new Identifier(GreatBigWorld.NAMESPACE, "black_stained_calcite"), drops(BlockRegistry.BLACK_STAINED_CALCITE));
+            biConsumer.accept(new Identifier(GreatBigWorld.NAMESPACE, "gray_stained_calcite"), drops(BlockRegistry.GRAY_STAINED_CALCITE));
+            biConsumer.accept(new Identifier(GreatBigWorld.NAMESPACE, "light_gray_stained_calcite"), drops(BlockRegistry.LIGHT_GRAY_STAINED_CALCITE));
+            biConsumer.accept(new Identifier(GreatBigWorld.NAMESPACE, "white_stained_calcite"), drops(BlockRegistry.WHITE_STAINED_CALCITE));
+        }
+
+        public LootTable.Builder drops(ItemConvertible drop) {
+            return LootTable.builder().pool(LootPool.builder().rolls(ConstantLootNumberProvider.create(1f)).with(ItemEntry.builder(drop)).getThisConditionConsumingBuilder());
         }
     }
 }
