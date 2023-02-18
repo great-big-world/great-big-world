dc42fc5a163db0e0ba64e31a72ac7956750b7790
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-02-17 22:26:42 -0500

    Placer Changes from main

--- src/main/java/com/github/creoii/greatbigworld/main/registry/PlacerRegistry.java
+++ src/main/java/com/github/creoii/greatbigworld/main/registry/PlacerRegistry.java
@@ -1,22 +1,24 @@
 package com.github.creoii.greatbigworld.main.registry;
 
+import com.github.creoii.greatbigworld.main.mixin.world.FoliagePlacerTypeInvoker;
+import com.github.creoii.greatbigworld.main.mixin.world.TreeDecoratorTypeInvoker;
+import com.github.creoii.greatbigworld.main.mixin.world.TrunkPlacerTypeInvoker;
 import com.github.creoii.greatbigworld.main.util.Register;
+import com.github.creoii.greatbigworld.world.decorator.BranchTreeDecorator;
 import com.github.creoii.greatbigworld.world.placer.AspenFoliagePlacer;
 import com.github.creoii.greatbigworld.world.placer.TwistingTrunkPlacer;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.foliage.FoliagePlacerType;
+import net.minecraft.world.gen.treedecorator.TreeDecoratorType;
 import net.minecraft.world.gen.trunk.TrunkPlacerType;
 
-import static com.github.creoii.greatbigworld.main.GreatBigWorld.NAMESPACE;
-
 public class PlacerRegistry implements Register {
-    public static final TrunkPlacerType<TwistingTrunkPlacer> TWISTING_TRUNK_PLACER = new TrunkPlacerType<>(TwistingTrunkPlacer.CODEC);
-    public static final FoliagePlacerType<AspenFoliagePlacer> ASPEN_FOLIAGE_PLACER = new FoliagePlacerType<>(AspenFoliagePlacer.CODEC);
+    public static TrunkPlacerType<TwistingTrunkPlacer> TWISTING_TRUNK_PLACER;
+    public static FoliagePlacerType<AspenFoliagePlacer> ASPEN_FOLIAGE_PLACER;
+    public static TreeDecoratorType<BranchTreeDecorator> BRANCH_TREE_DECORATOR;
 
     public void register() {
-        Registry.register(Registries.TRUNK_PLACER_TYPE, new Identifier(NAMESPACE, "twisting_trunk_placer"), TWISTING_TRUNK_PLACER);
-        Registry.register(Registries.FOLIAGE_PLACER_TYPE, new Identifier(NAMESPACE, "aspen_foliage_placer"), ASPEN_FOLIAGE_PLACER);
+        TWISTING_TRUNK_PLACER = TrunkPlacerTypeInvoker.callRegister("twisting_trunk_placer", TwistingTrunkPlacer.CODEC);
+        ASPEN_FOLIAGE_PLACER = FoliagePlacerTypeInvoker.callRegister("aspen_foliage_placer", AspenFoliagePlacer.CODEC);
+        BRANCH_TREE_DECORATOR = TreeDecoratorTypeInvoker.callRegister("branch", BranchTreeDecorator.CODEC);
     }
 }
