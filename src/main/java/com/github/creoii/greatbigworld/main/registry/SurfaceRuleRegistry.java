87486da1d3149dee6be8b5281a26e3a76136d447
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-02-22 16:08:37 -0500

    Edit Tags & Re-add Sand to Island Beaches

--- src/main/java/com/github/creoii/greatbigworld/main/registry/SurfaceRuleRegistry.java
+++ src/main/java/com/github/creoii/greatbigworld/main/registry/SurfaceRuleRegistry.java
@@ -10,7 +10,7 @@ import terrablender.api.SurfaceRuleManager;
 public class SurfaceRuleRegistry implements Register {
     private static final MaterialRules.MaterialRule LAVAROCK = MaterialRules.block(BlockRegistry.LAVAROCK.getDefaultState());
     private static final MaterialRules.MaterialRule GRASSY_LAVAROCK = MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR, MaterialRules.block(BlockRegistry.GRASSY_LAVAROCK.getDefaultState()));
-    private static final MaterialRules.MaterialRule BEACH_SAND = MaterialRules.sequence(MaterialRules.condition(MaterialRules.STONE_DEPTH_CEILING, MaterialRules.block(Blocks.SANDSTONE.getDefaultState())), MaterialRules.block(Blocks.SMOOTH_SANDSTONE.getDefaultState()));
+    private static final MaterialRules.MaterialRule BEACH_SAND = MaterialRules.sequence(MaterialRules.condition(MaterialRules.STONE_DEPTH_CEILING, MaterialRules.block(Blocks.SANDSTONE.getDefaultState())), MaterialRules.block(Blocks.SAND.getDefaultState()));
     private static final MaterialRules.MaterialRule SMOOTH_BASALT = MaterialRules.block(Blocks.SMOOTH_BASALT.getDefaultState());
 
     @Override
