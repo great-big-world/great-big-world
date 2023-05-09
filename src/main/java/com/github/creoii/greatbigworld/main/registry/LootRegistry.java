c34531dbb476366dc897937b492e1bdfb2e29381
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-05-09 17:59:38 -0400

    Music Disc in Buried Treasure

--- src/main/java/com/github/creoii/greatbigworld/main/registry/LootRegistry.java
+++ src/main/java/com/github/creoii/greatbigworld/main/registry/LootRegistry.java
@@ -3,17 +3,17 @@ package com.github.creoii.greatbigworld.main.registry;
 import com.github.creoii.greatbigworld.main.util.Register;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTables;
 import net.minecraft.loot.condition.LocationCheckLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.predicate.entity.LocationPredicate;
-import net.minecraft.util.Identifier;
 
 public class LootRegistry implements Register {
     @Override
     public void register() {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-            if (id.equals(new Identifier("minecraft", "chest/buried_treasure")) && source.isBuiltin()) {
+            if (id.equals(LootTables.BURIED_TREASURE_CHEST) && source.isBuiltin()) {
                 tableBuilder.pool(LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1f))
                         .with(ItemEntry.builder(ItemRegistry.MUSIC_DISC_PINA_COLADA))
