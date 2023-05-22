5d7a8959cb99517e55b0d37235348ac6ca2f988a
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-05-22 16:19:14 -0400

    Remove GBW Branch Tree Decorator

--- src/main/java/com/github/creoii/greatbigworld/main/registry/GBWDecorators.java
+++ src/main/java/com/github/creoii/greatbigworld/main/registry/GBWDecorators.java
@@ -3,18 +3,15 @@ package com.github.creoii.greatbigworld.main.registry;
 import com.github.creoii.greatbigworld.main.mixin.world.TreeDecoratorTypeInvoker;
 import com.github.creoii.greatbigworld.main.util.Register;
 import com.github.creoii.greatbigworld.world.decorator.AcaiBerriesTreeDecorator;
-import com.github.creoii.greatbigworld.world.decorator.BranchTreeDecorator;
 import com.github.creoii.greatbigworld.world.decorator.HangingLeavesTreeDecorator;
 import net.minecraft.world.gen.treedecorator.TreeDecoratorType;
 
 public class GBWDecorators implements Register {
-    public static TreeDecoratorType<BranchTreeDecorator> BRANCH_TREE_DECORATOR;
     public static TreeDecoratorType<AcaiBerriesTreeDecorator> ACAI_BERRIES_TREE_DECORATOR;
     public static TreeDecoratorType<HangingLeavesTreeDecorator> HANGING_LEAVES_TREE_DECORATOR;
 
     @Override
     public void register() {
-        BRANCH_TREE_DECORATOR = TreeDecoratorTypeInvoker.callRegister("branch", BranchTreeDecorator.CODEC);
         ACAI_BERRIES_TREE_DECORATOR = TreeDecoratorTypeInvoker.callRegister("acai_berries", AcaiBerriesTreeDecorator.CODEC);
         HANGING_LEAVES_TREE_DECORATOR = TreeDecoratorTypeInvoker.callRegister("hanging_leaves", HangingLeavesTreeDecorator.CODEC);
     }
