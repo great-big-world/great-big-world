4eea02464351c20e3302fe92e99b7ab535d364e6
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-05-22 16:31:30 -0400

    Couple fixes pre-release

--- src/main/java/com/github/creoii/greatbigworld/main/mixin/block/MagmaBlockMixin.java
+++ src/main/java/com/github/creoii/greatbigworld/main/mixin/block/MagmaBlockMixin.java
@@ -17,7 +17,7 @@ public class MagmaBlockMixin {
     @Inject(method = "randomTick", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILSOFT)
     private void great_big_world_magmaGeneratesLavarock(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci, BlockPos blockPos) {
         BlockState upState = world.getBlockState(pos.up());
-        if (!upState.isOf(Blocks.BUBBLE_COLUMN) && !upState.get(BubbleColumnBlock.DRAG)) return;
+        if (world.getBlockState(pos).isOf(Blocks.MAGMA_BLOCK) && upState.isOf(Blocks.BUBBLE_COLUMN) && !upState.get(BubbleColumnBlock.DRAG)) return;
 
         BlockPos checkPos = pos.add(random.nextInt(7) - 3, random.nextInt(4) - 1, random.nextInt(7) - 3);
         if (!world.getBlockState(checkPos.up()).isOf(Blocks.WATER)) return;
