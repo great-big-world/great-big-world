d81bd54766d9bd7f3361fd60fc505099441c8265
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-06-05 09:14:15 -0400

    Various Stuff
    might not be doing 1.2.0 for 1.19.2 anymore. why would I?

--- src/main/java/com/github/creoii/greatbigworld/main/registry/GBWBiomes.java
+++ src/main/java/com/github/creoii/greatbigworld/main/registry/GBWBiomes.java
@@ -8,25 +8,17 @@ import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.biome.Biome;
 
 public class GBWBiomes implements Register {
-    public static RegistryKey<Biome> ASPEN_FOREST;
-    public static RegistryKey<Biome> SNOWY_ASPEN_FOREST;
-    public static RegistryKey<Biome> ISLAND_JUNGLE;
-    public static RegistryKey<Biome> ISLAND_SPARSE_JUNGLE;
-    public static RegistryKey<Biome> ISLAND_BEACH;
+    public static final RegistryKey<Biome> ASPEN_FOREST = RegistryKey.of(Registry.BIOME_KEY, new Identifier(GreatBigWorld.NAMESPACE, "aspen_forest"));
+    public static final RegistryKey<Biome> SNOWY_ASPEN_FOREST = RegistryKey.of(Registry.BIOME_KEY, new Identifier(GreatBigWorld.NAMESPACE, "snowy_aspen_forest"));
+    public static final RegistryKey<Biome> ISLAND_JUNGLE = RegistryKey.of(Registry.BIOME_KEY, new Identifier(GreatBigWorld.NAMESPACE, "island_jungle"));
+    public static final RegistryKey<Biome> ISLAND_SPARSE_JUNGLE = RegistryKey.of(Registry.BIOME_KEY, new Identifier(GreatBigWorld.NAMESPACE, "island_sparse_jungle"));
+    public static final RegistryKey<Biome> ISLAND_BEACH = RegistryKey.of(Registry.BIOME_KEY, new Identifier(GreatBigWorld.NAMESPACE, "island_beach"));
     // island stony shores
-    public static RegistryKey<Biome> VOLCANIC_BEACH;
-    public static RegistryKey<Biome> VOLCANIC_SLOPES;
-    public static RegistryKey<Biome> VOLCANIC_CRATER;
+    public static final RegistryKey<Biome> VOLCANIC_BEACH = RegistryKey.of(Registry.BIOME_KEY, new Identifier(GreatBigWorld.NAMESPACE, "volcanic_beach"));
+    public static final RegistryKey<Biome> VOLCANIC_SLOPES = RegistryKey.of(Registry.BIOME_KEY, new Identifier(GreatBigWorld.NAMESPACE, "volcanic_slopes"));
+    public static final RegistryKey<Biome> VOLCANIC_CRATER = RegistryKey.of(Registry.BIOME_KEY, new Identifier(GreatBigWorld.NAMESPACE, "volcanic_crater"));
 
     @Override
     public void register() {
-        ASPEN_FOREST = RegistryKey.of(Registry.BIOME_KEY, new Identifier(GreatBigWorld.NAMESPACE, "aspen_forest"));
-        SNOWY_ASPEN_FOREST = RegistryKey.of(Registry.BIOME_KEY, new Identifier(GreatBigWorld.NAMESPACE, "snowy_aspen_forest"));
-        ISLAND_JUNGLE = RegistryKey.of(Registry.BIOME_KEY, new Identifier(GreatBigWorld.NAMESPACE, "island_jungle"));
-        ISLAND_SPARSE_JUNGLE = RegistryKey.of(Registry.BIOME_KEY, new Identifier(GreatBigWorld.NAMESPACE, "island_sparse_jungle"));
-        ISLAND_BEACH = RegistryKey.of(Registry.BIOME_KEY, new Identifier(GreatBigWorld.NAMESPACE, "island_beach"));
-        VOLCANIC_BEACH = RegistryKey.of(Registry.BIOME_KEY, new Identifier(GreatBigWorld.NAMESPACE, "volcanic_beach"));
-        VOLCANIC_SLOPES = RegistryKey.of(Registry.BIOME_KEY, new Identifier(GreatBigWorld.NAMESPACE, "volcanic_slopes"));
-        VOLCANIC_CRATER = RegistryKey.of(Registry.BIOME_KEY, new Identifier(GreatBigWorld.NAMESPACE, "volcanic_crater"));
     }
 }
