1865fc594736343b83cc77a93c93d0b7ac302c08
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-06-21 01:55:56 -0400

    Biome Placement
    Dynamic region size
    Move Wisterias to only Blooming Flower Forest
    Cracked Red Rock Bricks

--- src/main/java/com/github/creoii/greatbigworld/main/integration/TerraBlenderIntegration.java
+++ src/main/java/com/github/creoii/greatbigworld/main/integration/TerraBlenderIntegration.java
@@ -1,12 +1,13 @@
 package com.github.creoii.greatbigworld.main.integration;
 
 import com.github.creoii.greatbigworld.world.region.GBWOverworldRegion;
+import terrablender.api.RegionType;
 import terrablender.api.Regions;
 import terrablender.api.TerraBlenderApi;
 
 public class TerraBlenderIntegration implements TerraBlenderApi {
     @Override
     public void onTerraBlenderInitialized() {
-        Regions.register(new GBWOverworldRegion(3));
+        Regions.register(new GBWOverworldRegion(2 * Regions.getCount(RegionType.OVERWORLD)));
     }
 }
--- src/main/java/com/github/creoii/greatbigworld/world/region/GBWOverworldRegion.java
+++ src/main/java/com/github/creoii/greatbigworld/world/region/GBWOverworldRegion.java
@@ -43,24 +43,38 @@ public class GBWOverworldRegion extends Region {
                     .weirdness(Weirdness.MID_SLICE_NORMAL_ASCENDING, Weirdness.HIGH_SLICE_NORMAL_ASCENDING, Weirdness.HIGH_SLICE_NORMAL_DESCENDING, Weirdness.MID_SLICE_NORMAL_DESCENDING, Weirdness.LOW_SLICE_NORMAL_DESCENDING, Weirdness.VALLEY, Weirdness.LOW_SLICE_VARIANT_ASCENDING, Weirdness.MID_SLICE_VARIANT_ASCENDING, Weirdness.HIGH_SLICE_VARIANT_ASCENDING, Weirdness.HIGH_SLICE_VARIANT_DESCENDING, Weirdness.MID_SLICE_VARIANT_DESCENDING)
                     .offset(0f)
                     .build().forEach(point -> builder.replaceBiome(point, GBWBiomes.SNOWY_ASPEN_FOREST));
-            /*new ParameterUtils.ParameterPointListBuilder()
-                    .temperature(Temperature.NEUTRAL)
-                    .humidity(Humidity.NEUTRAL)
-                    .continentalness(Continentalness.COAST, Continentalness.NEAR_INLAND).continentalness(Continentalness.span(Continentalness.COAST, Continentalness.NEAR_INLAND), Continentalness.span(Continentalness.NEAR_INLAND, Continentalness.FAR_INLAND), Continentalness.span(Continentalness.MID_INLAND, Continentalness.FAR_INLAND), Continentalness.span(Continentalness.COAST, Continentalness.FAR_INLAND))
-                    .erosion(Erosion.EROSION_2, Erosion.EROSION_3, Erosion.EROSION_4, Erosion.EROSION_6).erosion(Erosion.span(Erosion.EROSION_0, Erosion.EROSION_1), Erosion.span(Erosion.EROSION_2, Erosion.EROSION_3))
-                    .depth(Depth.SURFACE, Depth.FLOOR)
-                    .weirdness(Weirdness.MID_SLICE_NORMAL_ASCENDING, Weirdness.HIGH_SLICE_NORMAL_ASCENDING, Weirdness.PEAK_NORMAL, Weirdness.HIGH_SLICE_NORMAL_DESCENDING, Weirdness.MID_SLICE_NORMAL_DESCENDING, Weirdness.LOW_SLICE_NORMAL_DESCENDING, Weirdness.VALLEY, Weirdness.LOW_SLICE_VARIANT_ASCENDING, Weirdness.MID_SLICE_VARIANT_ASCENDING, Weirdness.HIGH_SLICE_VARIANT_ASCENDING, Weirdness.PEAK_VARIANT, Weirdness.HIGH_SLICE_VARIANT_DESCENDING, Weirdness.MID_SLICE_VARIANT_DESCENDING)
-                    .offset(0f)
-                    .build().forEach(point -> builder.replaceBiome(point, BiomeRegistry.BLOOMING_FLOWER_FOREST));
-             */
             new ParameterUtils.ParameterPointListBuilder()
-                    .temperature(Temperature.NEUTRAL)
+                    .temperature(Temperature.NEUTRAL, Temperature.COOL)
                     .humidity(Humidity.ARID, Humidity.NEUTRAL)
                     .continentalness(Continentalness.span(Continentalness.MID_INLAND, Continentalness.FAR_INLAND))
                     .erosion(Erosion.EROSION_2, Erosion.EROSION_3)
                     .depth(Depth.SURFACE, Depth.FLOOR)
                     .weirdness(Weirdness.MID_SLICE_NORMAL_ASCENDING, Weirdness.HIGH_SLICE_NORMAL_ASCENDING, Weirdness.HIGH_SLICE_NORMAL_DESCENDING, Weirdness.MID_SLICE_NORMAL_DESCENDING, Weirdness.LOW_SLICE_NORMAL_DESCENDING, Weirdness.LOW_SLICE_VARIANT_ASCENDING, Weirdness.MID_SLICE_VARIANT_ASCENDING, Weirdness.HIGH_SLICE_VARIANT_ASCENDING, Weirdness.HIGH_SLICE_VARIANT_DESCENDING, Weirdness.MID_SLICE_VARIANT_DESCENDING)
                     .build().forEach(point -> builder.replaceBiome(point, GBWBiomes.HOT_SPRINGS));
+            new ParameterUtils.ParameterPointListBuilder()
+                    .temperature(Temperature.NEUTRAL)
+                    .humidity(Humidity.ARID)
+                    .continentalness(Continentalness.NEAR_INLAND, Continentalness.MID_INLAND, Continentalness.FAR_INLAND, Continentalness.COAST).continentalness(Continentalness.span(Continentalness.COAST, Continentalness.NEAR_INLAND)).continentalness(Continentalness.span(Continentalness.NEAR_INLAND, Continentalness.FAR_INLAND)).continentalness(Continentalness.span(Continentalness.MID_INLAND, Continentalness.FAR_INLAND))
+                    .erosion(Erosion.EROSION_3, Erosion.FULL_RANGE, Erosion.EROSION_6, Erosion.EROSION_4).erosion(Erosion.span(Erosion.EROSION_1, Erosion.EROSION_5)).erosion(Erosion.span(Erosion.EROSION_1, Erosion.EROSION_4)).erosion(Erosion.span(Erosion.EROSION_3, Erosion.EROSION_4)).erosion(Erosion.span(Erosion.EROSION_1, Erosion.EROSION_6)).erosion(Erosion.span(Erosion.EROSION_0, Erosion.EROSION_5))
+                    .depth(Depth.SURFACE, Depth.FLOOR)
+                    .weirdness(Weirdness.MID_SLICE_NORMAL_ASCENDING, Weirdness.HIGH_SLICE_NORMAL_ASCENDING, Weirdness.HIGH_SLICE_NORMAL_DESCENDING, Weirdness.LOW_SLICE_NORMAL_DESCENDING).weirdness(Weirdness.span(Weirdness.MID_SLICE_NORMAL_ASCENDING, Weirdness.MID_SLICE_NORMAL_DESCENDING)).weirdness(Weirdness.span(Weirdness.MID_SLICE_NORMAL_ASCENDING, Weirdness.LOW_SLICE_NORMAL_DESCENDING)).weirdness(Weirdness.span(Weirdness.HIGH_SLICE_NORMAL_ASCENDING, Weirdness.HIGH_SLICE_NORMAL_DESCENDING)).weirdness(Weirdness.span(Weirdness.HIGH_SLICE_NORMAL_ASCENDING, Weirdness.LOW_SLICE_NORMAL_DESCENDING)).weirdness(Weirdness.span(Weirdness.PEAK_NORMAL, Weirdness.HIGH_SLICE_NORMAL_DESCENDING)).weirdness(Weirdness.span(Weirdness.MID_SLICE_NORMAL_DESCENDING, Weirdness.LOW_SLICE_NORMAL_DESCENDING))
+                    .build().forEach(point -> builder.replaceBiome(point, GBWBiomes.BLOOMING_FLOWER_FOREST));
+            new ParameterUtils.ParameterPointListBuilder()
+                    .temperature(Temperature.HOT)
+                    .humidity(Humidity.span(Humidity.ARID, Humidity.NEUTRAL)).humidity(Humidity.DRY)
+                    .continentalness(Continentalness.NEAR_INLAND, Continentalness.MID_INLAND, Continentalness.FAR_INLAND).continentalness(Continentalness.span(Continentalness.MID_INLAND, Continentalness.FAR_INLAND)).continentalness(Continentalness.span(Continentalness.NEAR_INLAND, Continentalness.FAR_INLAND))
+                    .erosion(Erosion.EROSION_0, Erosion.EROSION_1, Erosion.EROSION_2).erosion(Erosion.span(Erosion.EROSION_1, Erosion.EROSION_2)).erosion(Erosion.span(Erosion.EROSION_1, Erosion.EROSION_3))
+                    .depth(Depth.SURFACE, Depth.FLOOR)
+                    .weirdness(Weirdness.MID_SLICE_NORMAL_ASCENDING, Weirdness.HIGH_SLICE_NORMAL_ASCENDING, Weirdness.HIGH_SLICE_NORMAL_DESCENDING, Weirdness.MID_SLICE_NORMAL_DESCENDING, Weirdness.MID_SLICE_VARIANT_ASCENDING, Weirdness.HIGH_SLICE_VARIANT_ASCENDING, Weirdness.HIGH_SLICE_VARIANT_DESCENDING, Weirdness.MID_SLICE_VARIANT_DESCENDING).weirdness(Weirdness.span(Weirdness.MID_SLICE_NORMAL_ASCENDING, Weirdness.MID_SLICE_NORMAL_DESCENDING)).weirdness(Weirdness.span(Weirdness.HIGH_SLICE_NORMAL_ASCENDING, Weirdness.HIGH_SLICE_NORMAL_DESCENDING)).weirdness(Weirdness.span(Weirdness.PEAK_NORMAL, Weirdness.HIGH_SLICE_NORMAL_DESCENDING)).weirdness(Weirdness.span(Weirdness.MID_SLICE_VARIANT_ASCENDING, Weirdness.HIGH_SLICE_VARIANT_ASCENDING)).weirdness(Weirdness.span(Weirdness.MID_SLICE_VARIANT_ASCENDING, Weirdness.HIGH_SLICE_VARIANT_ASCENDING)).weirdness(Weirdness.span(Weirdness.MID_SLICE_VARIANT_ASCENDING, Weirdness.MID_SLICE_VARIANT_DESCENDING)).weirdness(Weirdness.span(Weirdness.HIGH_SLICE_VARIANT_ASCENDING, Weirdness.HIGH_SLICE_VARIANT_DESCENDING)).weirdness(Weirdness.span(Weirdness.PEAK_VARIANT, Weirdness.HIGH_SLICE_VARIANT_DESCENDING)).weirdness(Weirdness.span(Weirdness.HIGH_SLICE_VARIANT_DESCENDING, Weirdness.MID_SLICE_VARIANT_DESCENDING)).weirdness(Weirdness.span(Weirdness.LOW_SLICE_VARIANT_ASCENDING, Weirdness.MID_SLICE_VARIANT_ASCENDING))
+                    .build().forEach(point -> builder.replaceBiome(point, GBWBiomes.RED_ROCK_SLOPES));
+            new ParameterUtils.ParameterPointListBuilder()
+                    .temperature(Temperature.HOT)
+                    .humidity(Humidity.FULL_RANGE, Humidity.ARID)
+                    .continentalness(Continentalness.FAR_INLAND, Continentalness.MID_INLAND, Continentalness.NEAR_INLAND).continentalness(Continentalness.span(Continentalness.MID_INLAND, Continentalness.FAR_INLAND)).continentalness(Continentalness.span(Continentalness.NEAR_INLAND, Continentalness.FAR_INLAND))
+                    .erosion(Erosion.EROSION_0, Erosion.EROSION_1).erosion(Erosion.span(Erosion.EROSION_0, Erosion.EROSION_1)).erosion(Erosion.span(Erosion.EROSION_2, Erosion.EROSION_3)).erosion(Erosion.span(Erosion.EROSION_1, Erosion.EROSION_3))
+                    .depth(Depth.SURFACE, Depth.FLOOR)
+                    .weirdness(Weirdness.MID_SLICE_NORMAL_ASCENDING, Weirdness.PEAK_NORMAL, Weirdness.MID_SLICE_NORMAL_DESCENDING, Weirdness.MID_SLICE_VARIANT_ASCENDING, Weirdness.PEAK_VARIANT, Weirdness.MID_SLICE_VARIANT_DESCENDING, Weirdness.LOW_SLICE_VARIANT_ASCENDING).weirdness(Weirdness.span(Weirdness.HIGH_SLICE_NORMAL_ASCENDING, Weirdness.HIGH_SLICE_NORMAL_DESCENDING)).weirdness(Weirdness.span(Weirdness.HIGH_SLICE_VARIANT_ASCENDING, Weirdness.MID_SLICE_VARIANT_DESCENDING)).weirdness(Weirdness.span(Weirdness.LOW_SLICE_VARIANT_ASCENDING, Weirdness.MID_SLICE_VARIANT_ASCENDING)).weirdness(Weirdness.span(Weirdness.MID_SLICE_VARIANT_ASCENDING, Weirdness.MID_SLICE_VARIANT_DESCENDING))
+                    .build().forEach(point -> builder.replaceBiome(point, GBWBiomes.RED_ROCK_PEAKS));
         });
     }
 }
