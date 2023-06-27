7837fe9041b34c5121765ad6b41a244f3b736f0f
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-06-27 12:18:04 -0400

    Fix Recipe Book Category Order
    Update dependencies
    Nether Brick Kiln Recipe

--- src/main/java/com/github/creoii/greatbigworld/main/mixin/recipe/RecipeBookGroupMixin.java
+++ src/main/java/com/github/creoii/greatbigworld/main/mixin/recipe/RecipeBookGroupMixin.java
@@ -3,6 +3,7 @@ package com.github.creoii.greatbigworld.main.mixin.recipe;
 import com.github.creoii.greatbigworld.main.util.GBWRecipeBookCategories;
 import com.github.creoii.greatbigworld.main.util.GBWRecipeBookGroups;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.recipebook.RecipeBookGroup;
 import net.minecraft.item.ItemStack;
@@ -22,6 +23,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Mixin(RecipeBookGroup.class)
 public class RecipeBookGroupMixin {
@@ -36,21 +38,26 @@ public class RecipeBookGroupMixin {
     @Mutable
     private static RecipeBookGroup[] field_1805;
 
-    private static final List<RecipeBookGroup> KILN = ImmutableList.of(GBWRecipeBookGroups.KILN_MISC, GBWRecipeBookGroups.KILN_BLOCKS);
+    @Mutable
+    @Shadow @Final public static Map<RecipeBookGroup, List<RecipeBookGroup>> SEARCH_MAP;
+    private static final List<RecipeBookGroup> KILN = ImmutableList.of(GBWRecipeBookGroups.KILN_SEARCH, GBWRecipeBookGroups.KILN_BLOCKS, GBWRecipeBookGroups.KILN_MISC);
 
     @Inject(method = "<clinit>", at = @At(value = "FIELD", opcode = Opcodes.PUTSTATIC, target = "Lnet/minecraft/client/recipebook/RecipeBookGroup;field_1805:[Lnet/minecraft/client/recipebook/RecipeBookGroup;", shift = At.Shift.AFTER))
     private static void addCustomRecipeBookGroup(CallbackInfo ci) {
         ArrayList<RecipeBookGroup> types = new ArrayList<>(Arrays.asList(field_1805));
         RecipeBookGroup last = types.get(types.size() - 1);
 
-        RecipeBookGroup kilnMisc = create("GBW_KILN_MISC", last.ordinal() + 1, new ItemStack(Items.BRICK));
+        RecipeBookGroup kilnSearch = create("GBW_KILN_SEARCH", last.ordinal() + 1, new ItemStack(Items.COMPASS));
         RecipeBookGroup kilnBlocks = create("GBW_KILN_BLOCKS", last.ordinal() + 2, new ItemStack(Blocks.BROWN_GLAZED_TERRACOTTA));
-        RecipeBookGroup sawmill = create("GBW_SAWMILL", last.ordinal() + 3, new ItemStack(Blocks.OAK_PLANKS));
-        GBWRecipeBookGroups.KILN_MISC = kilnMisc;
+        RecipeBookGroup kilnMisc = create("GBW_KILN_MISC", last.ordinal() + 3, new ItemStack(Items.BRICK));
+        RecipeBookGroup sawmill = create("GBW_SAWMILL", last.ordinal() + 4, new ItemStack(Blocks.OAK_PLANKS));
+        GBWRecipeBookGroups.KILN_SEARCH = kilnSearch;
         GBWRecipeBookGroups.KILN_BLOCKS = kilnBlocks;
+        GBWRecipeBookGroups.KILN_MISC = kilnMisc;
         GBWRecipeBookGroups.SAWMILL = sawmill;
-        types.add(kilnMisc);
+        types.add(kilnSearch);
         types.add(kilnBlocks);
+        types.add(kilnMisc);
         types.add(sawmill);
 
         field_1805 = types.toArray(new RecipeBookGroup[0]);
@@ -61,5 +68,12 @@ public class RecipeBookGroupMixin {
         if (category == GBWRecipeBookCategories.KILN) {
             cir.setReturnValue(KILN);
         }
+    }
+
+    static {
+        SEARCH_MAP = new ImmutableMap.Builder<RecipeBookGroup, List<RecipeBookGroup>>()
+                .putAll(SEARCH_MAP)
+                .put(GBWRecipeBookGroups.KILN_SEARCH, ImmutableList.of(GBWRecipeBookGroups.KILN_BLOCKS, GBWRecipeBookGroups.KILN_MISC))
+                .build();
     }
 }
--- src/main/java/com/github/creoii/greatbigworld/main/util/GBWRecipeBookGroups.java
+++ src/main/java/com/github/creoii/greatbigworld/main/util/GBWRecipeBookGroups.java
@@ -3,8 +3,9 @@ package com.github.creoii.greatbigworld.main.util;
 import net.minecraft.client.recipebook.RecipeBookGroup;
 
 public class GBWRecipeBookGroups {
-    public static RecipeBookGroup KILN_MISC;
+    public static RecipeBookGroup KILN_SEARCH;
     public static RecipeBookGroup KILN_BLOCKS;
+    public static RecipeBookGroup KILN_MISC;
     public static RecipeBookGroup SAWMILL;
 
     static {
